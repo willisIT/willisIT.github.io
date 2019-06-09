58a6bc86a5b63a8d78b6d80918bf14abd30def22
willisIT <williamskyei24@gmail.com>
2019-06-09 18:01:07 +0100

    initial commit

---
+++ precache-manifest.0a8e63be551817170022feaeba46f4c4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3f506529063c3f5a1aebfc29aec37fe",
+    "url": "/present.jpg"
+  },
+  {
+    "revision": "b6522e7271f9da678882b6cf14cbeb3b",
+    "url": "/ruby.jpg"
+  },
+  {
+    "revision": "e8f72ef4f8270a467af7f1749f94601b",
+    "url": "/vb.jpg"
+  },
+  {
+    "revision": "9c6b49fe73dd49bc850facd95a043405",
+    "url": "/sm.jpg"
+  },
+  {
+    "revision": "bf225e9504bc12a9ce73fbbf9616cc8c",
+    "url": "/seo.jpg"
+  },
+  {
+    "revision": "d55e9c4f653efab40974",
+    "url": "/css/chunk-vendors.95f6c680.css"
+  },
+  {
+    "revision": "d4017045310db93afb12",
+    "url": "/js/about.787ca9cc.js"
+  },
+  {
+    "revision": "f6e739bcdce8af79cc1960c13b0ad613",
+    "url": "/py.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a64680217218a63c5bead28f19491c07",
+    "url": "/ps.jpg"
+  },
+  {
+    "revision": "9138bbcb6c4984e1ddd533e0e536f755",
+    "url": "/pp.jpg"
+  },
+  {
+    "revision": "a1b1c14a470c52f4bc0a58457702d26e",
+    "url": "/mobile.jpg"
+  },
+  {
+    "revision": "b6f7783b0ad2b7a0f05153d434466742",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "6b038127fe8fdbd493bb",
+    "url": "/js/app.80d11b56.js"
+  },
+  {
+    "revision": "d55e9c4f653efab40974",
+    "url": "/js/chunk-vendors.059a6999.js"
+  },
+  {
+    "revision": "f9f88e4cce299c0637690bb1608e050a",
+    "url": "/project.jpg"
+  },
+  {
+    "revision": "196520b03c4f62c1eef7a4871d108765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d230f10751c8f80b853450d8f9e2e2",
+    "url": "/ga.jpg"
+  },
+  {
+    "revision": "20dfff69e8385537779d9b7b32853479",
+    "url": "/excel.png"
+  },
+  {
+    "revision": "8f7301b8f4877b12c961757cc00add4f",
+    "url": "/data.png"
+  },
+  {
+    "revision": "6b038127fe8fdbd493bb",
+    "url": "/css/app.0c8207ae.css"
+  },
+  {
+    "revision": "ee3b3c2b922ff972d4034d594399d551",
+    "url": "/c.jpg"
+  },
+  {
+    "revision": "fc6e7f2d683e988e0c4955b086079a9d",
+    "url": "/bg.jpg"
+  },
+  {
+    "revision": "47a6e414b07b3962ad3ecc4e0997f197",
+    "url": "/af.jpg"
+  },
+  {
+    "revision": "6472f6ad0237633d096bdc46a6b660e7",
+    "url": "/ae.jpg"
+  },
+  {
+    "revision": "67837fb69ea26c1a693dccfe4d23f74e",
+    "url": "/ad.jpg"
+  }
+];
